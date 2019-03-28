29aafaeaf88d37192f996fd06893554918cc458f
zeynep <sungjin@zeynep-mbp.local>
2019-03-28 11:45:34 -0400

    after eject

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
 
     return(
-        <div className="Person" >
+        <div className={classes.Person} >
             <p onClick={props.click}>My name is {props.name} and I am {props.age} old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
